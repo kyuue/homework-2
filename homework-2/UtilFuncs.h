37a6d6925083f0acc5b3b9ea6d43f64b124a70ba
schoolworksacc <61066894+schoolworksacc@users.noreply.github.com>
2020-03-09 17:25:22 +0300

    Update UtilFuncs.h

--- homework-2/UtilFuncs.h
+++ homework-2/UtilFuncs.h
@@ -2,27 +2,3 @@
 
 #include <string>
 
-template <class Container>
-void split(const std::string& str, Container& cont, // http://www.martinbroadhurst.com/how-to-split-a-string-in-c.html
-	std::string delims, int index = 0);
-
-
-
-
-template<class Container>
-inline void split(const std::string & str, Container & cont, std::string delims, int index)
-{
-	if (index >= delims.size())
-		return;
-
-	std::size_t current, previous = 0;
-	current = str.find(delims[index]);
-	while (current != std::string::npos) {
-		cont.push_back(str.substr(previous, current - previous));
-		previous = current + 1;
-		current = str.find(delims, previous);
-	}
-	cont.push_back(str.substr(previous, current - previous));
-
-	split(str, cont, delims, ++index);
-}
