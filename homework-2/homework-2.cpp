6c92a9bd6b0ab09ae8db7af166f8323a9a387e3f
schoolworksacc <61066894+schoolworksacc@users.noreply.github.com>
2020-03-09 17:26:06 +0300

    Update homework-2.cpp

--- homework-2/homework-2.cpp
+++ homework-2/homework-2.cpp
@@ -9,7 +9,11 @@
 
 int main()
 {
-	ComplexNumber complexNumber1, complexNumber2 = 1/* conversion constructor */, complexNumber3 = 1.0, complexNumber4("34- 24i"), complexNumber5(5, 1);
+	ComplexNumber test(1);
+
+	ComplexNumber complexNumber1, complexNumber2 = 1/* conversion constructor */, complexNumber3 = 1.0, complexNumber4("34- 24i"), complexNumber5(34, -23);
+
+	ComplexNumber complexNumber8 = std::string("34 - 24i");
 
 	complexNumber1.SetRealPart(-5);
 
@@ -25,5 +29,22 @@ int main()
 
 	complexNumber1 = complexNumber1 - complexNumber2;
 
-	complexNumber1 = complexNumber1 / complexNumber2;
+	complexNumber1 = complexNumber1 / complexNumber4;
+
+	std::cout << std::endl;
+
+	complexNumber5.Show();
+
+	std::cout << "\n> -- list --\n";
+
+	std::vector<ComplexNumber> numberList;
+
+	numberList.push_back(complexNumber5);
+	numberList.push_back(complexNumber4);
+	numberList.push_back(complexNumber3);
+	numberList.push_back(complexNumber2);
+	numberList.push_back(complexNumber1);
+
+	ComplexNumber::sortAndPrint(numberList);
+
 }
